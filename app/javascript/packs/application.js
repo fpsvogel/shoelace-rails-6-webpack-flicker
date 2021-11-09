478bcfd7d3de687c8dae1ca06290d01f4fa2c69a
Felipe Vogel <fps.vogel@gmail.com>
2021-11-08 20:45:16 -0500

    init

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,21 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+import { setBasePath } from '@shoelace-style/shoelace'
+
+const rootUrl = document.currentScript.src.replace(/\/packs.*$/, '')
+
+// Path to the assets folder (should be independent from the current script source path
+// to work correctly in different environments)
+setBasePath(rootUrl + '/packs/js/')
